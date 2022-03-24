af4641232f11f9f639477a9fc9adb5f626b2bcee
Ahmad20 <44915914+Ahmad20@users.noreply.github.com>
2022-03-24 18:12:30 +0700

    Update init.R

--- init.R
+++ init.R
@@ -15,13 +15,3 @@ install_if_missing = function(p) {
 invisible(sapply(my_packages, install_if_missing))
 
 ###########################################################
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
-  }
-  else {
-    cat(paste("Skipping already installed package:", p, "\n"))
-  }
-}
-invisible(sapply(my_packages, install_if_missing))
