56992e2a5f0a4588c08a4510a343236be8c144c9
Ahmad20 <44915914+Ahmad20@users.noreply.github.com>
2022-03-24 18:20:13 +0700

    Update run.R

--- run.R
+++ run.R
@@ -4,7 +4,7 @@ library(shinydashboardPlus)
 library(tidyverse)
 library(plotly)
 library(europepmc)
-#source("function.R")
+source("function.R")
 ### Default shop list
 shop_list <- c("nestle.official.store", "indofooddapurcitarasa", 
                "unileverindonesia", "wingsofficialshop", "pgofficialstore")
@@ -16,6 +16,121 @@ for (shop in shop_list){
   shop_df[nrow(shop_df)+1,] <- set_shop(shop)
 }
 #shop_df
+
+shinyUI(
+  dashboardPage(
+    
+    skin = "midnight",
+    #title = "FMCG Indonesia | Tetris 2022",
+    title = "FMCG Indonesia | Tetris 2022",
+    dashboardHeader(
+      title = tagList(
+        tags$span(
+          class = "logo-mini", "AJT",
+        ),
+        tags$span(
+          class = "logo-lg", "Tetris 2022 | DQLab",
+        )
+      )
+    ),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem(
+          text = "Dashboard", 
+          tabName = "page1",
+          badgeLabel = "New!", 
+          badgeColor = "green",
+          icon = icon("bar-chart")
+        )
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(
+          tabName = "page1",
+          
+          fluidRow(
+            box(
+              title = "Overview Dashboard", 
+              closable = FALSE, 
+              enable_label = TRUE,
+              label_status = "danger",
+              status = "primary", 
+              solidHeader = FALSE, 
+              collapsible = TRUE,
+              width = 12,
+              
+              infoBoxOutput(
+                outputId = "nshop", 
+                width = 4
+              ),
+              
+              infoBoxOutput(
+                outputId = "popularshop",
+                width = 4
+              ),
+              
+              infoBoxOutput(
+                outputId = "ratingshop",
+                width = 4
+              )
+            )
+          ),
+          
+          fluidRow(
+            
+            box(
+              title = "Select Shop Name", 
+              closable = FALSE, 
+              enable_label = TRUE,
+              label_status = "danger",
+              status = "primary", 
+              solidHeader = FALSE, 
+              collapsible = TRUE,
+              width = 3,
+              height = "600px",
+              
+              selectInput(
+                inputId = "shopname", 
+                label = "Select Shop Name", 
+                choices = c(shop_df$Shopname),
+                selected = "Wings Official Shop"
+              ),
+              selectInput(
+                inputId = "typegraph", 
+                label = "Select Type of Graph", 
+                choices = c("Penjualan terbanyak",
+                            "Korelasi harga - rating", 
+                            "Korelasi harga - terjual", 
+                            "Korelasi rating - terjual"),
+                selected = "Wings Official Shop"
+              ),
+              plotlyOutput(
+                outputId = "pie"
+              )
+            ),
+            
+            box(
+              title = "Graph", 
+              closable = FALSE, 
+              enable_label = TRUE,
+              label_status = "danger",
+              status = "primary", 
+              solidHeader = FALSE, 
+              collapsible = TRUE,
+              width = 9,
+              height = "600px",
+              
+              plotlyOutput(
+                outputId = "graph"
+              )
+            )
+          ) 
+        )
+      )
+    )
+  )
+)
 
 server <- shinyServer(function(input, output, session) {
   output$nshop <- renderInfoBox({
@@ -109,11 +224,4 @@ server <- shinyServer(function(input, output, session) {
   })
   
 })
-
-port <- Sys.getenv('PORT')
-
-shiny::runApp(
-  appDir = getwd(),
-  host = '0.0.0.0',
-  port = as.numeric(port)
-)
+shinyApp(ui, server)
